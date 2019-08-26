4495dfcd47526a63361235ec3915a0c972dc2b4d
David Forer <david.j.forer@gmail.com>
2019-08-26 06:14:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,8 +1,7 @@
 require_relative 'config/environment'
-require "pry"
 
  class App < Sinatra::Base
 
-   get '/reversename/:name' do
-    params[:name].reverse
-  end
+    get '/reversename/:name' do
+      params[:name].reverse
+    end
